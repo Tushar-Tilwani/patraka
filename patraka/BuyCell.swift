379338ba7703654e88e0c64c0e6af1004733298f
tushar <tushar.tilwani@live.in>
2016-05-01 23:27:23 -0400

    before bluetooth

---
+++ patraka/BuyCell.swift
@@ -0,0 +1,24 @@
+//
+//  BuyCell.swift
+//  patraka
+//
+//  Created by Tushar Tilwani on 4/29/16.
+//  Copyright © 2016 Tushar Tilwani. All rights reserved.
+//
+
+import UIKit
+
+class BuyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ patraka/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  constants.swift
+//  patraka
+//
+//  Created by Tushar Tilwani on 4/29/16.
+//  Copyright © 2016 Tushar Tilwani. All rights reserved.
+//
+
+import Foundation
